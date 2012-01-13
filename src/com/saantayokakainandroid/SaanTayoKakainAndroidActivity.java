00a70ef404188aaa63ec31c252efc25e9e5e13cd
mareksison <mareksison@yahoo.com>
2012-01-13 23:10:49 +0800

    Added filter's 2 spinners, initiated them, and fixed the variable name of filter's button On branch master 	modified:   src/com/saantayokakainandroid/SaanTayoKakainAndroidActivity.java

--- src/com/saantayokakainandroid/SaanTayoKakainAndroidActivity.java
+++ src/com/saantayokakainandroid/SaanTayoKakainAndroidActivity.java
@@ -3,7 +3,9 @@ package com.saantayokakainandroid;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * 
@@ -13,16 +15,28 @@ import android.widget.Button;
  */
 public class SaanTayoKakainAndroidActivity extends Activity {
 	
-	private Button generateB;
+	private Button filterB;
+	private Spinner costS;
+	private Spinner cuisineS;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.filter);
     }
     
     public void initUI(){
     	
+    	costS = (Spinner) findViewById(R.id.costS);
+    	cuisineS = (Spinner) findViewById(R.id.costS);
+    	
+        ArrayAdapter<CharSequence> costSAdapter = ArrayAdapter.createFromResource(this, R.array.costArray, android.R.layout.simple_spinner_item);
+        costSAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        costS.setAdapter(costSAdapter);
+        ArrayAdapter<CharSequence> cuisineSAdapter = ArrayAdapter.createFromResource(this, R.array.cuisineArray, android.R.layout.simple_spinner_item);
+        cuisineSAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cuisineS.setAdapter(cuisineSAdapter);
+
     }
 }
