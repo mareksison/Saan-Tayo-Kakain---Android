83b3abfcbc92df4ebd110784a742806e7455e447
mareksison <mareksison@yahoo.com>
2012-01-13 22:46:43 +0800

    initial commit  On branch master 	new file:   AndroidManifest.xml 	new file:   res/drawable-hdpi/ic_launcher.png 	new file:   res/drawable-ldpi/ic_launcher.png 	new file:   res/drawable-mdpi/ic_launcher.png 	new file:   res/drawable-mdpi/saanTayoKakainCheck.png 	new file:   res/layout/achieve.xml 	new file:   res/layout/filter.xml 	new file:   res/layout/main.xml 	new file:   res/layout/view.xml 	new file:   res/values/strings.xml 	new file:   src/com/saantayokakainandroid/SaanTayoKakainAndroidActivity.java

---
+++ src/com/saantayokakainandroid/SaanTayoKakainAndroidActivity.java
@@ -0,0 +1,26 @@
+package com.saantayokakainandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * 
+ * @author mareksison
+ * @version 0.0
+ * @since January 11, 2012
+ */
+public class SaanTayoKakainAndroidActivity extends Activity {
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    public void initUI(){
+    	
+    }
+}
