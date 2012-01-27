94785c5a328863fa004370fcd486b693eab30d2b
Aron Asor <aronasorman@gmail.com>
2012-01-28 03:54:17 +0800

    removed unused imports from ViewActivity  On branch 2.0 	modified:   ViewActivity.java

--- src/com/saantayokakainandroid/ViewActivity.java
+++ src/com/saantayokakainandroid/ViewActivity.java
@@ -1,9 +1,5 @@
 package com.saantayokakainandroid;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
